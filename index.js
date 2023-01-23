7ef0e792f45d575e583a877d966ddb2ca944f451
Bruna0092 <50524997+Bruna0092@users.noreply.github.com>
2023-01-23 16:18:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ try {
   const args = minimist(process.argv.slice(2))
 
   if (args['help'] || args['h']) {
-    throw "Expected usage: node index.js --dir=\"./working-dir\" --hostname=\"./apiUrl\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\""
+    throw "Expected usage: node index.js --dir=\"./working-dir\" --environment=\"./apiUrl\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\""
   }
 
-  const requiredArgs = ['dir', 'hostname', 'graph', 'token']
+  const requiredArgs = ['dir', 'environment', 'graph', 'token']
 
   if (Object.keys(args).length <= requiredArgs.length) {
-    throw `Expected at least ${requiredArgs.length} cli arguments. usage: node index.js --dir=\"./working-dir\" --hostname=\"./apiUrl\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\"`
+    throw `Expected at least ${requiredArgs.length} cli arguments. usage: node index.js --dir=\"./working-dir\" --environment=\"./apiUrl\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\"`
   }
 
   requiredArgs.forEach(requiredArg => {
@@ -26,7 +26,7 @@ try {
   })
 
   const tfWorkingDir = args['dir']
-  const apiUrl = args['hostname']
+  const apiUrl = args['environment']
   const tfPlan = args['plan']
   const tfcToken = args['tfc-token']
   const tfcRunId = args['tfc-run-id']
